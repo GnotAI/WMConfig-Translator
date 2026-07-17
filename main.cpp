b0d66780046e802c1cc65027d38b0edf6d3fd25c
GnotAI <alleluiabriggs@gmail.com>
2026-07-17 18:17:30 +0100

    upd: changed prompt help feed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <typeinfo>
 #include "includes/conf_utils.h"
 #include "includes/hyprland.h"
+#include "includes/niri.h"
+#include "window_manager.h"
 
 int main (int argc, char *argv[]) {
 
@@ -9,7 +12,8 @@ int main (int argc, char *argv[]) {
 
   std::cout << "------------ Start parsing. -------------" << '\n';
 
-  generateFullFile(hypr);
+  std::cout << typeid(windowManagerArg).name() << '\n';
+  generateFullFile(wm);
 
   std::cout << "------------ Done parsing. -------------" << '\n';
 
